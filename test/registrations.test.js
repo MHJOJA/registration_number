31b6c49c70952f21f6e6e980596d97c64462dbc4
Momelezi Joja <dmenzi694@gmail.com>
2020-11-30 09:18:11 +0200

    modified tests

--- test/registrations.test.js
+++ test/registrations.test.js
@@ -29,7 +29,7 @@ describe("The registration numbers webapp", function () {
 
   
     
-      // it('should be able to return   if its  from Cape Town', async function(){
+      // it('should be able to return numberplate if its from Cape Town', async function(){
       //   await regNumbers.insertRegNumbers('CY 321 456');
       //   await regNumbers.insertRegNumbers('CA 987 125');
       //   await regNumbers.insertRegNumbers('CL 985 748');
@@ -37,18 +37,28 @@ describe("The registration numbers webapp", function () {
       //     {numberplates : 'CA 987 123'}
       //   ])
       // })
-it('should be able to reset the database and clear the rnumberplates', async function () {
-        await regNumbers.insertRegNumbers("CA 321-541")
-        await regNumbers.insertRegNumbers("Cy 785 214")
-        await regNumbers.insertRegNumbers("CA 321")
+it('should be able to reset the database ', async function () {
+        await regNumbers.insertRegNumbers("CA 321-541");
+        await regNumbers.insertRegNumbers("Cy 785 214");
+        await regNumbers.insertRegNumbers("CA 321");
         
 		await regNumbers.reset()
 
-		const results = await pool.query("select count(*) from registration_numbers");
+		const results = await pool.query("select count (*) from registration_numbers");
 
 		await assert.deepEqual(0, results.rows[0].count)
-	})
-})
+  })
+  
+//   it('should be able to add registration numbers from different towns',async function(){
+//     await regNumbers.insertRegNumbers('CA 132 654')
+//     await regNumbers.insertRegNumbers('CL 789-325')
+
+//     const results = await regNumbers.insertRegNumbers()
+// 		await assert.deepEqual([{reg_numb: 'CA 132 654'},{reg_numb: 'CL 789325'}], results)
+
+//   })
+
+ })
 
 
 
